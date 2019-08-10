1dea83c8b30430762dae49dc591abb6bbade8d9e
ELOL <elizarov1999@list.ru>
2019-08-10 22:32:50 +0300

    изменено:      polls/templates/polls/diagramm.html 	изменено:      polls/templates/polls/index.html 	изменено:      polls/urls.py 	изменено:      polls/views.py 	изменено:      templates/registration/logged_out.html 	изменено:      templates/registration/login.html 	изменено:      templates/registration/password_reset_complete.html 	изменено:      templates/registration/password_reset_confirm.html 	изменено:      templates/registration/password_reset_done.html 	изменено:      templates/registration/password_reset_email.html

--- site33/settings.py
+++ site33/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'site33.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),'./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/polls'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
