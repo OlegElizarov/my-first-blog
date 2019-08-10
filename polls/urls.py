6b6543468466b1b4cde3313a88cfd67c0446cd1e
ELOL <elizarov1999@list.ru>
2019-08-10 14:19:33 +0300

    изменено:      polls/templates/polls/diagramm.html 	изменено:      polls/templates/polls/index.html 	изменено:      polls/urls.py 	изменено:      polls/views.py 	изменено:      site33/urls.py 	новый файл:    templates/registration/login.html

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,9 @@ from polls import views
 app_name="polls"
 
 urlpatterns = [
-        url(r'^$', views.IndexView.as_view(), name='index'),
+        url(r'^$', views.index, name='index'),
         url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
         url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
         url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+        url(r'^(?P<question_id>\d+)/usr/$', views.IndexView.as_view(), name='ind'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from polls.models import Question , Choice
+from polls.models import Question, Choice
 from django.views import generic
 from django.utils import timezone
 
@@ -9,6 +9,7 @@ from django.utils import timezone
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+
 
     def get_queryset(self):
         return Question.objects.filter(
@@ -19,30 +20,49 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-def vote (request, question_id):
-        p = get_object_or_404(Question, pk=question_id)
-        try:
-            selected_choice = p.choice_set.get(pk=request.POST['choice'])
-        except (KeyError, Choice.DoesNotExist):
-            # Redisplay the question voting form.
-            return render(request, 'polls/detail.html', {
-                'question': p,
-                'error_message': "You didn't select a choice.",
-            })
-        else:
-            selected_choice.votes += 1
-            selected_choice.save()
-            # Always return an HttpResponseRedirect after successfully dealing
-            # with POST data. This prevents data from being posted twice if a
-            # user hits the Back button.
-            return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request,
+            'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        }
+                      )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+
+def index(request):
+    p = Question
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(
+        request,
+        'polls/index.html',
+        context={'num_visits': num_visits , 'question': p,
+                 'latest_question_list': latest_question_list}
+        )
