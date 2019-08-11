a42d02b9aeaeec963eeca8ca33c5e1bb7e8bdc53
ELOL <elizarov1999@list.ru>
2019-08-11 20:14:09 +0300

    изменено:      polls/admin.py 	новый файл:    polls/migrations/0002_question_done_by.py 	изменено:      polls/models.py 	изменено:      polls/static/polls/style.css 	изменено:      polls/templates/polls/index.html

--- polls/models.py
+++ polls/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
+    done_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
 
@@ -18,6 +22,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.PROTECT)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
-
